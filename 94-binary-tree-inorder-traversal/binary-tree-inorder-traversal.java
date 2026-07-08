54a2ea154d602d19952f3f3a9da67126cd97e075
SWATI SINGH <swatisingh.cg@gmail.com>
2026-07-08 18:33:06 +0530

    Time: 1 ms (6.36%) | Memory: 43.3 MB (32.95%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            ans.add(curr.val);
+
+            curr = curr.right;
+        }
+
+        return ans;
+    }
+}
