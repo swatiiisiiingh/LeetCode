c4b24729ebb74784b3eef692c628b595e7f69985
SWATI SINGH <swatisingh.cg@gmail.com>
2026-07-19 20:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (65.47%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,48 +1,24 @@
-class Solution {
-
-    public int search(int[] nums, int target) {
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            // Target found
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            // Left half sorted
-            if (nums[left] <= nums[mid]) {
-
-                // Target inside left half
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
+class Solution{
+    public int search(int[] nums, int target){
+        int low=0, high=nums.length-1;
+        while (low<=high){
+            int mid = (low+high)/2;
+            if (nums[mid]==target)
+              return mid;
+            if (nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
                 }
-
-                // Search right half
-                else {
-                    left = mid + 1;
+            }else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
                 }
-            }
-
-            // Right half sorted
-            else {
-
-                // Target inside right half
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                }
-
-                // Search left half
-                else {
-                    right = mid - 1;
-                }
-            }
+            }  
         }
-
         return -1;
     }
 }
